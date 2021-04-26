af35f77f1e80ba660ffe20b0b9c30bc5b9fb544d
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-04-26 04:32:05 +0300

    feat: сompleted the class for working with fractions

---
+++ Learn Objective-C/02. Классы, обьекты и методы/prog-for-working-with-fractions/prog-for-working-with-fractions/main.m
@@ -0,0 +1,63 @@
+//
+//  main.m
+//  prog-for-working-with-fractions
+//
+//  Created by Егор Горских on 26.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//--Секция @interface--
+
+@interface Fraction : NSObject
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+//--Секция @implementation--
+
+@implementation Fraction
+-(void) print
+{
+NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d
+{
+    denominator = d;
+}
+
+@end
+
+//--Секция @program--
+int main(int argc, const char * argv[]) {
+    
+    @autoreleasepool {
+        Fraction *myFraction;
+        
+        // Создание экземпляра
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        // Присваивание значения 1/3
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator: 3];
+        
+        // Выводим в принт
+        NSLog(@"Значение myFraction:");
+        [myFraction print];
+    }
+    return 0;
+}
