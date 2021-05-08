00a8b8a0ef9979e4e42a083c8e81529fd5727faf
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-05-08 11:51:28 +0300

    init: start methods

---
+++ Skutarenko/Properties/Properties/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Properties
+//
+//  Created by Egor Gorskikh on 08.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
